c90437e464bd3320947f56835b54296b33315145
KadyshevIV <otmorozok73@list.ru>
2018-05-14 23:30:13 +0400

    partial task execution

--- task1/Program.cs
+++ task1/Program.cs
@@ -10,7 +10,22 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow Word!");
+            Console.WriteLine("Введите размерность массива");
+            int n = int.Parse(Console.ReadLine());
+            
+            int[,] mass = new int[n, n];
+
+
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    mass[i, j] = -1;
+                    //mass[i, j] = 0;
+                    Console.Write("{0,3}", mass[i, j]);
+                }
+                Console.WriteLine();
+            }
             Console.ReadKey();
         }
     }
